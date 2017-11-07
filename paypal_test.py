6287772ccbe587a216787cdacf14be4a1365347b
Becca Rosenthal <beccarosenthal@gmail.com>
2017-11-07 20:50:10 +0000

    reorganizaed server, built part of generating payment object fn in paypal_test.py

--- paypal_test.py
+++ paypal_test.py
@@ -1,100 +1,119 @@
+from model import (User, Organization, Transaction,
+                   User_Org, State,
+                   connect_to_db, db)
+
 from paypalrestsdk import Payment, configure
 import os
 
 client_id = os.environ.get("PAYPAL_CLIENT_ID")
 client_secret = os.environ.get("PAYPAL_CLIENT_SECRET")
 
+
 api = configure({"mode": "sandbox",
                          "client_id": client_id,
                          "client_secret": client_secret})
 
 
+#TODO figure out what needs to passed in in order to connect this to db
 # Create payment object
-payment = Payment({
-    "intent": "sale",
+def generate_payment_object(user_id, org_id):
+    """generate payment object"""
 
-  # Set payment method
-    "payer": {
-      "payer_info": {"email": "beccarosenthal-buyer@gmail.com",
-                     "first_name": "Becca",
-                     "last_name": "Rosenthal",
-                     },
+    #TODO Write queries that will get info about the payer, payee, and transaction
+    #from/for the database
 
-      "payment_method": "paypal",
-      },
+    user_obj = User.query.filter(User.user_id == user_id).one()
+    org_obj = Organization.query.filter(Organization.org_id == org_id).one()
 
-      # Set redirect urls
-      "redirect_urls": {
-        "return_url": "/process",
-        "cancel_url": "/cancel"
-      },
 
+
+    #Generate Payment Object
+    payment = Payment({
+        "intent": "sale",
+
+      # Set payment method
+        "payer": {
+
+        #TODO make payer_info come from db
+            "payer_info": {"email": user_obj.user_email,
+                         "first_name": user_obj.fname,
+                         "last_name": user_obj.lname,
+                         },
+
+            "payment_method": "paypal",
+            },
+
+        # Set redirect urls
+        "redirect_urls": {
+            "return_url": "/process",
+            "cancel_url": "/cancel"
+            },
+      #TODO make connect this to db
       # Set transaction object
-      "transactions": [{
-        "amount": {
-          "total": "10.00",
-          "currency": "USD"
-        },
-        "description": "test donation",
+        "transactions": [{
+            "amount": {
+                "total": user_obj.default_amount,
+                "currency": "USD"
+                },
+            "description": "test donation",
 
-        "payee": {
-                  'email': "beccarosenthal-facilitator@gmail.com",
-                  'payee_display_metadata':
-                    {
-                    'brand_name':
-                    "This is an ORG"
-                    },
-                  },
+            #TODO make payee info come from db
+            "payee": {
+                      'email': FACILITATOR_EMAIL,
+                      'payee_display_metadata':
+                        {'brand_name': "This is an ORG"},
+                      },
 
-      }
-      ]
-    })
+          }
+          ]
+        })
+    import pdb; pdb.set_trace()
+      # Create payment
+    if payment.create():
+        # Extract redirect url
+        for link in payment.links:
+          if link.method == "REDIRECT":
+            # Capture redirect url
+            redirect_url = str(link.href)
 
-
-# Create payment
-if payment.create():
-  # Extract redirect url
-  for link in payment.links:
-    if link.method == "REDIRECT":
-      # Capture redirect url
-      redirect_url = str(link.href)
-
-      # REDIRECT USER to redirect_url
-else:
-  print("Error while creating payment:")
-  print(payment.error)
+            # REDIRECT USER to redirect_url
+    else:
+        print("Error while creating payment:")
+        print(payment.error)
 
 ##WHEN YOU GO TO THAT LINK< IT ACTUALLY WORKS
 
 
+
+if __name__ == "__main__":
 ###payment is a dictionary, representing the transaction between person being paid and doing paying
   # Payment id obtained when creating the payment (following redirect)
-payment = Payment.find("PAY-28103131SP722473WKFD7VGQ")
+    payment = Payment.find("PAY-28103131SP722473WKFD7VGQ")
 
-# Execute payment using payer_id obtained when creating the payment (following redirect)
-if payment.execute({"payer_id": "DUFRQ8GWYMJXC"}):
-  print("Payment[%s] execute successfully" % (payment.id))
-else:
-  print(payment.error)
-
-
-from paypalrestsdk import Payment
-
-# Payment id obtained when creating the payment (following redirect)
-payment = Payment.find("PAY-28103131SP722473WKFD7VGQ")
-
-# Execute payment using payer_id obtained when creating the payment (following redirect)
-if payment.execute({"payer_id": "DUFRQ8GWYMJXC"}):
-  print("Payment[%s] execute successfully" % (payment.id))
-else:
-  print(payment.error)
+    # Execute payment using payer_id obtained when creating the payment (following redirect)
+    if payment.execute({"payer_id": "DUFRQ8GWYMJXC"}):
+      print("Payment[%s] execute successfully" % (payment.id))
+    else:
+      print(payment.error)
 
 
-# Payment id obtained when creating the payment (following redirect)
-payment = Payment.find("PAY-28103131SP722473WKFD7VGQ")
+    from paypalrestsdk import Payment
 
-# Execute payment using payer_id obtained when creating the payment (following redirect)
-if payment.execute({"payer_id": "DUFRQ8GWYMJXC"}):
-  print("Payment[%s] execute successfully" % (payment.id))
-else:
-  print(payment.error)
+    # Payment id obtained when creating the payment (following redirect)
+    payment = Payment.find("PAY-28103131SP722473WKFD7VGQ")
+
+    # Execute payment using payer_id obtained when creating the payment (following redirect)
+    if payment.execute({"payer_id": "DUFRQ8GWYMJXC"}):
+      print("Payment[%s] execute successfully" % (payment.id))
+    else:
+      print(payment.error)
+
+
+    # Payment id obtained when creating the payment (following redirect)
+    payment = Payment.find("PAY-28103131SP722473WKFD7VGQ")
+
+    # Execute payment using payer_id obtained when creating the payment (following redirect)
+    if payment.execute({"payer_id": "DUFRQ8GWYMJXC"}):
+      print("Payment[%s] execute successfully" % (payment.id))
+    else:
+      print(payment.error)
--- server.py
+++ server.py
@@ -1,16 +1,22 @@
 """DESPAIR CHANGE"""
+#python standard libraries
+import datetime
+import os
 
-
-from jinja2 import StrictUndefined
-
+#third party things
 from flask import (Flask, render_template, redirect, request, flash,
                    session, jsonify)
-# from model import User, Rating, Movie, connect_to_db, db
 from flask_debugtoolbar import DebugToolbarExtension
-
+from jinja2 import StrictUndefined
 from paypalrestsdk import Payment, configure
-import os
-import datetime
+
+
+#import from my files
+from model import (User, Organization, Transaction,
+                   User_Org, State,
+                   connect_to_db, db)
+from paypal_test import generate_payment_object, api
+
 
 app = Flask(__name__)
 app.secret_key = 'werewolf-bar-mitzvah'
@@ -18,11 +24,78 @@ app.secret_key = 'werewolf-bar-mitzvah'
 client_id = os.environ.get("PAYPAL_CLIENT_ID")
 client_secret = os.environ.get("PAYPAL_CLIENT_SECRET")
 
+
+# General templates
+###############################################################################
+
+
 @app.route('/')
 def index():
     """renders homepage"""
 
     return render_template('homepage.html')
+
+
+
+
+@app.route('/about')
+def show_about_page():
+    """renders about Despair Change page"""
+
+    return render_template('about.html')
+
+
+@app.route('/dashboard')
+def show_user_dashboard():
+    """show user dashboard"""
+
+    return render_template('dashboard.html')
+
+
+# Registration and login logic functions
+###############################################################################
+
+
+@app.route('/register')
+def show_registration_form():
+    """render registration form"""
+
+    return render_template('register.html')
+
+
+@app.route('/register', methods=['POST'])
+def process_registration():
+    """extract data from reg form, add user to database, redirect
+    to donate page with login added to session"""
+
+    #TODO add logic about making sure they type a password that matches -
+    # it may be on the html template, not here
+    user_email = request.form.get('email')
+    user_password = request.form.get('password')
+    fname = request.form.get('fname')
+    lname = request.form.get('lname')
+    age = request.form.get('age')
+    zipcode = request.form.get('zipcode')
+    state = request.form.get('state')
+    phone = request.form.get('phone')
+
+    user_object = User.query.filter(User.email == user_email).first()
+
+    #If user object with email address provided doens't exist, add to db...
+    if not user_object :
+        new_user = User(user_email=user_email,
+                        password=user_password,
+                        fname=fname,
+                        lname=lname,
+                        age=age,
+                        zipcode=zipcode,
+                        state_code=state,
+                        phone=phone)
+        db.session.add(new_user)
+        db.session.commit()
+
+    session['current_user'] = user_object.user_id
+    return redirect('/login')
 
 
 @app.route('/login')
@@ -45,24 +118,23 @@ def login_user():
     user_password = request.form.get('password')
 
     #get the user object from the email
-    user_object = User.query.filter(User.email == user_email).first()
-    #
-    if user_object:
+    user_object = User.query.filter(User.user_email == user_email).first()
 
+    if user_object:
     #check password against email address
         if user_object.password == user_password:
 
-            #update user.last_login in database
-            user_object.last_login = datetime.datetime.utcnow
-            db.session.commit()
+            #TODO make last login work
+            # #update user.last_login in database
+            # user_object.last_login = datetime.datetime.utcnow
+            # db.session.commit()
 
             flash("You're logged in. Welcome to Despair Change!!")
             specific_user_id = user_object.user_id
             session['current_user'] = specific_user_id
 
             #What is the specific user ID
-            url = '/users/' + str(specific_user_id)
-            return redirect(url)
+            return redirect('/donate')
 
         else:
             flash("That is an incorrect password")
@@ -72,80 +144,6 @@ def login_user():
         flash('You need to register first!')
         return redirect('/register.html')
 
-    flash('for now, we\'ll assume you\'re actually logged in')
-    return redirect('/donate')
-
-    # #while we don't have db up and running, let's just assume user logged in
-    # session['current_user'] = 'logged_in'
-
-@app.route('/register')
-def show_registration_form():
-    """render registration form"""
-
-    return render_template('register.html')
-
-
-@app.route('/register', methods=['POST'])
-def process_registration():
-    """extract data from reg form, add user to database, redirect
-    to donate page with login added to session"""
-
-    user_email = request.form.get('email')
-    user_password = request.form.get('password')
-    fname = request.form.get('fname')
-    lname = request.form.get('lname')
-    age = request.form.get('age')
-    zipcode = request.form.get('zipcode')
-    state = request.form.get('state')
-    phone = request.form.get('phone')
-
-    user_object = User.query.filter(User.email == user_email).first()
-
-    #If user object with email address provided doens't exist, add to db...
-    if not user_object::
-        new_user = User(email=user_email,
-                        password=user_password,
-                        fname=fname,
-                        lname=lname,
-                        age=age,
-                        zipcode=zipcode,
-                        state_code=state,
-                        phone=phone)
-        db.session.add(new_user)
-        db.session.commit()
-
-    session['current_user'] = user_object.user_id
-    return redirect('/login')
-
-
-@app.route('/about')
-def show_about_page():
-    """renders about page"""
-
-    return render_template('about.html')
-
-
-@app.route('/dashboard')
-def show_user_dashboard():
-    """show user dashboard"""
-
-    return render_template('dashboard.html')
-
-
-@app.route('/donate')
-def donation_page():
-    """render page to donate"""
-
-    return render_template('donate.html')
-
-
-@app.route('/donated', methods=['POST'])
-def process_donation():
-
-
-    flash('I got redirected here from the paypal button!')
-    return redirect('/dashboard')
-
 
 @app.route('/logout')
 def logout_user():
@@ -154,6 +152,47 @@ def logout_user():
     del session['current_user']
     flash('successfully logged out...50-50 odds')
     return redirect ('/')
+
+
+@app.route('/cancel')
+def cancel_payment():
+    """cancels payment"""
+
+
+    flash('I think I just canceled a payment')
+    return redirect('/')
+
+
+#routes about paypal/payment things
+###############################################################################
+@app.route('/donate')
+def donation_page():
+    """render page to donate"""
+
+    #TODO - when project develops into multiple orgs, target_org-->list
+    org = Organization.query.filter(Organization.org_id == 1).one()
+    return render_template('donate.html', org=org)
+
+
+@app.route('/donated', methods=['POST'])
+def process_donation():
+
+    user_id = session['current_user']
+    org_id = request.form.get('org')
+
+    print "user_id=", user_id
+    print "org_id=", org_id
+
+    payment_object = generate_payment_object(user_id, org_id)
+
+    ##Generate payment obj using payer info from session, org info from form
+
+    ##generate Transaction object to go into db
+
+    flash('I got redirected here from the paypal button!')
+    return redirect('/dashboard', payment_object)
+
+
 
 
 @app.route('/buttons')
@@ -174,14 +213,6 @@ def process_payment():
     return redirect('/')
 
 
-@app.route('/cancel')
-def cancel_payment():
-    """cancels payment"""
-
-
-    flash('I think I just canceled a payment')
-    return redirect('/')
-
 
 
 
@@ -191,7 +222,7 @@ if __name__ == "__main__":
     app.debug = True
     app.jinja_env.auto_reload = app.debug  # make sure templates, etc. are not cached in debug mode
 
-    # connect_to_db(app)
+    connect_to_db(app)
 
     # Use the DebugToolbar
     DebugToolbarExtension(app)
