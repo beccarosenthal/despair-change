2206aa265e890fa1846111749b456cd30c51c10a
Becca Rosenthal <beccarosenthal@gmail.com>
2017-11-23 04:58:06 +0000

    aesthetic changes

--- static/js/base.js
+++ static/js/base.js
@@ -8,9 +8,29 @@ $('#donationModal').on('show.bs.modal', function (event) {
   console.log(recipient)
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this)
+  var modal = $(this);
   console.log("i'm here...")});
 
-$('.carousel').carousel({
-  interval: 2000
-})
+$.getJSON("https://bootswatch.com/api/3.json", function (data) {
+  var themes = data.themes;
+  var select = $("select");
+  select.show();
+  $(".alert").toggleClass("alert-info alert-success");
+  $(".alert h4").text("Success!");
+
+  themes.forEach(function(value, index){
+    select.append($("<option />")
+          .val(index)
+          .text(value.name));
+  });
+
+  select.change(function(){
+    var theme = themes[$(this).val()];
+    $("link").attr("href", theme.css);
+    $("h1").text(theme.name);
+  }).change();
+
+}, "json").fail(function(){
+    $(".alert").toggleClass("alert-info alert-danger");
+    $(".alert h4").text("Failure!");
+});
