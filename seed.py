b83a4caad325b20c8a95364b2c351c415fcccbe6
Hackbright Student <no-reply@hackbrightacademy.com>
2017-12-04 13:10:56 -0800

    fake transactions and users added

--- seed.py
+++ seed.py
@@ -11,6 +11,7 @@ from server import app
 
 fake = Faker()
 ORG_IDS = [8, 9, 13, 15, 16]
+USER_IDS = [27, 15, 17, 22, 23, 16, 30, 20, 31, 32, 33]
 
 
 def load_states():
@@ -38,29 +39,29 @@ def load_states():
     print "States Loaded"
 
 
-def create_users():
-    """Creates users data from Faker."""
+# def create_users():
+#     """Creates users data from Faker."""
 
-    print 'Users'
+#     print 'Users'
 
-    with open('data/users.txt', 'w+') as users:
-        for i in range(500):
-            users.write('{}|{}|{}|{}|{}|{}|{}|{}|{}|{}\n'.format(fake.first_name(),
-                                                                 fake.last_name(),
-                                                                 fake.free_email(),
-                                                                 fake.boolean(
-                                                                     chance_of_getting_true=75),
-                                                                 fake.password(length=10,
-                                                                               special_chars=True,
-                                                                               digits=True,
-                                                                               upper_case=True,
-                                                                               lower_case=True),
-                                                                 random.choice(range(1, 6)),
-                                                                 fake.state_abbr(),
-                                                                 fake.zipcode(),
-                                                                 fake.phone_number(),
-                                                                 fake.date_time_this_year(before_now=True, after_now=False, tzinfo=None),
-                                                                 ))
+#     with open('data/users.txt', 'w+') as users:
+#         for i in range(500):
+#             users.write('{}|{}|{}|{}|{}|{}|{}|{}|{}|{}\n'.format(fake.first_name(),
+#                                                                  fake.last_name(),
+#                                                                  fake.free_email(),
+#                                                                  fake.boolean(
+#                                                                      chance_of_getting_true=75),
+#                                                                  fake.password(length=10,
+#                                                                                special_chars=True,
+#                                                                                digits=True,
+#                                                                                upper_case=True,
+#                                                                                lower_case=True),
+#                                                                  random.choice(range(1, 6)),
+#                                                                  fake.state_abbr(),
+#                                                                  fake.zipcode(),
+#                                                                  fake.phone_number(),
+#                                                                  fake.date_time_this_year(before_now=True, after_now=False, tzinfo=None),
+#                                                                  ))
 
 
 ##This is making everything error out right now because set password field isn't in 
@@ -68,20 +69,20 @@ def create_users():
 def add_users():
     """takes fake users and adds them to database"""
 
-    with open("data/users.txt") as users_file:
+    with open("data/users.csv") as users_file:
         for row in users_file:
             row = row.rstrip()
-            fname, lname, user_email, set_password, password, default_amount, state, zipcode, phone, created_at = row.split("|")
+            fname, lname, user_email, password, default_amount, state, zipcode, phone, created_at, set_password  = row.split(",")
             created_at = datetime.datetime.strptime(created_at, "%Y-%m-%d %H:%M:%S")
             user = User(fname=fname, 
                         lname=lname, 
                         user_email=user_email, 
                         set_password=set_password, 
                         password=password, 
-                        default_amount=default_amount,
+                        default_amount=float(default_amount),
                         state_code=state,
-                        zipcode=zipcode,
-                        # phone=phone,
+                        zipcode=str(zipcode),
+                        phone=phone,
                         created_at=created_at
                         )
 
@@ -99,8 +100,12 @@ def create_transactions():
     with open('data/transactions.txt', 'w+') as transactions:
         for i in range(1000):
             rand_num = random.randrange(0, 5)
-            user_id = random.choice(range(1, 500))
-            transactions.write("{}|{}|{}|{}|{}|\n".format(
+            #Make sure user_id chosen actually exists
+            user_id = random.choice(range(1, 1000))
+            while User.query.get(user_id) is None:
+                user_id = random.choice(range(1, 1000))
+
+            transactions.write("{}|{}|{}|{}|{}\n".format(
                 ORG_IDS[rand_num], #random org ID
                 user_id, #random user
                 User.query.get(user_id).default_amount,
@@ -108,6 +113,24 @@ def create_transactions():
                 fake.date_time_this_year(before_now=True, after_now=False, tzinfo=None),
                  )  )
                                 
+def add_transactions():
+    """add fake transactions to db"""
+
+    with open('data/transactions.txt') as transactions:
+        for row in transactions:
+            row = row.rstrip()
+            org_id, user_id, amount, payment_id, timestamp = row.split('|')
+            timestamp = datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+            transaction = Transaction(user_id=user_id,
+                org_id=org_id,
+                amount=float(amount),
+                timestamp=timestamp,
+                status='pending delivery to org',
+                payment_id=payment_id)
+
+            db.session.add(transaction)
+
+        db.session.commit()
 
 
 if __name__ == "__main__":
@@ -116,8 +139,9 @@ if __name__ == "__main__":
     # In case tables haven't been created, create them
     db.create_all()
     # load_states()
-    create_users()
-    add_users() #right now this will error out VERY badly
-    # create_transactions()
+    # create_users()
+    # add_users()
+    create_transactions()
+    add_transactions()
     # # Import different types of data
 
